35f0e9fccfefc961eea1fa9eae93f121140e9473
JuliaLerebäck-JU22GB <julia.corell@gmail.com>
2022-09-13 11:51:07 +0200

    Add Point.java

---
+++ src/se/iths/Main.java
@@ -0,0 +1,18 @@
+package se.iths;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point point1 = new Point();
+        Point point2 = new Point(1,1);
+        Point point3 = new Point(point2);
+
+        double distance = point1.distanceTo(point2);
+
+        System.out.println(distance);
+    }
+
+
+
+
+}
