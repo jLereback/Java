be741dfdd0540bdf8367237261a51ed0d7ac3780
julia <julia.lereback@gmail.com>
2022-09-21 10:13:34 +0200

    Add Records

--- src/se/iths/records/RPerson.java
+++ src/se/iths/records/RPerson.java
@@ -1,5 +1,8 @@
 package se.iths.records;
 
-public record RPerson(String firstName,
-                      String lastName,
-                      int age) {}
+public record RPerson(String firstName, String lastName, int age) {
+
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+}
--- src/se/iths/records/RecordsDemo.java
+++ src/se/iths/records/RecordsDemo.java
@@ -1,14 +1,25 @@
 package se.iths.records;
 
+import se.iths.records.Person;
+import se.iths.records.RPerson;
+
 public class RecordsDemo {
 
     public static void main(String[] args) {
-        Person donald = new Person("Donald", "Duck", 88);
+        Person donald = new Person("Donald","Duck",88);
+
         System.out.println(donald);
         System.out.println(donald.getFirstName());
 
-        RPerson donaldAsRecord = new RPerson("Donald", "Duck", 88);
+        RPerson donaldAsRecord = new RPerson("Donald","Duck",88);
         System.out.println(donaldAsRecord);
         System.out.println(donaldAsRecord.firstName());
+
+        printPersonPretty(donaldAsRecord);
+    }
+
+    public static void printPersonPretty(RPerson person) {
+        System.out.println(person.getName());
+        System.out.println("-------------");
     }
 }
