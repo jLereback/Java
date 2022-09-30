b3a4492491a08a878a5dccc0187e2b90601fa975
julia <julia.lereback@gmail.com>
2022-09-30 15:14:23 +0200

    Add task7_6

--- src/se/iths/tasks/tasks7/task7_6/Main.java
+++ src/se/iths/tasks/tasks7/task7_6/Main.java
@@ -1,19 +1,33 @@
 package se.iths.tasks.tasks7.task7_6;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Stack<String> myStack = new Stack<>();
+        myStack.push("First string");
+        myStack.push("Second string");
+        myStack.push("Third string");
+        System.out.println(myStack.size()); //3
+        System.out.println(myStack.pop());  //Third string
+        System.out.println(myStack.pop());  //Second string
+        System.out.println(myStack.pop());  //First string
 
-        Stack<Integer> list = new Stack<>();
-        System.out.println(list.size());
-        list.push(12);
-        System.out.println(list.size());
-        list.push(17);
+//        myStack.print();
+//        myStack.printReversed();
 
-        System.out.println(list.size());
-        System.out.println(list.pop());
-        System.out.println(list.size());
-        System.out.println(list.pop());
-        System.out.println(list.size());
 
+        System.out.println(myStack.size()); //0
+        System.out.println(myStack.isEmpty()); //true
+        // if (!myStack.isEmpty())
+        System.out.println(myStack.pop());  //Exception
+
+        List<String> strings = List.of("Hej");
+
+        for( String  s  : strings) {
+            System.out.println(s);
+        }
     }
 }
--- src/se/iths/tasks/tasks7/task7_6/Stack.java
+++ src/se/iths/tasks/tasks7/task7_6/Stack.java
@@ -3,20 +3,68 @@ package se.iths.tasks.tasks7.task7_6;
 import java.util.LinkedList;
 
 public class Stack<E> {
-    private final LinkedList<E> list = new LinkedList<>();
+
+    private Node<E> head;
+    private Node<E> tail;
 
     private int length;
+
     public void push(E value) {
-        int i = 0;
-        list.addLast(value);
+        if (isEmpty()) {
+            head = new Node<E>();
+            tail = head;
+            head.value = value;
+        } else {
+            var tempNode = head;
+            head = new Node<E>();
+            head.value = value;
+            tempNode.prev = head;
+            head.next = tempNode;
+        }
         length++;
     }
+
     public E pop() {
+        if (isEmpty())
+            return null;
+        E tempValue = head.value;
         length--;
-        return list.removeLast();
+        head = head.next;
+        if (head != null)
+            head.prev = null;
+
+        return tempValue;
+
     }
 
     public int size() {
         return length;
     }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void print() {
+        var temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    //For reversed order to work, we need a double linked list
+    public void printReversed() {
+        var temp = tail;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.prev;
+        }
+    }
+}
+
+class Node<T> {
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
 }
