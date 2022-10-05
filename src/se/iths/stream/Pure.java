55d17e450e5d79a89c642759b23df8178346195a
julia <julia.lereback@gmail.com>
2022-10-05 09:35:44 +0200

    Add Stream

---
+++ src/se/iths/stream/Pure.java
@@ -0,0 +1,62 @@
+package se.iths.stream;
+
+import java.security.PublicKey;
+import java.time.LocalDateTime;
+import java.util.function.Consumer;
+
+public class Pure {
+
+    static int counter = 0;
+
+    private int fieldValue;
+
+    public int getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(int fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public int getAndSetFieldValue(int fieldValue) {
+        int temp = this.fieldValue;
+        this.fieldValue = fieldValue;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dayOfWeek());
+
+        String data = "Some random text";
+
+        higherOrderFunction(text -> System.out.println(text + data));
+    }
+
+    public static void higherOrderFunction(Consumer<String> consumer) {
+        consumer.accept("Handling this using an implementation handed to us as argument");
+    }
+
+
+    public static int pureFunction(int a) {
+        return a * 2;
+    }
+
+    public static int sideEffects(int a) {
+        counter++;
+        return a;
+    }
+
+    public static int nonPureFunction(int a) {
+        return (int) (a * (Math.random() * 100));
+    }
+
+    public static int dayOfWeek() {
+        return LocalDateTime.now().getDayOfWeek().getValue();
+    }
+
+    public static String firstMonthOfTheYear() {
+        return "January";
+    }
+
+
+}
---
+++ src/se/iths/stream/StreamExamples.java
@@ -0,0 +1,92 @@
+package se.iths.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.util.function.Predicate.not;
+
+public class StreamExamples {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<String> stringList = List.of("Apple", "Apelsin", "Citrus", "Tomato", "Kiwi", "Grape");
+        List<Cat> catList = List.of(
+                new Cat("Misse", 10, true),
+                new Cat("Pelle", 2, false),
+                new Cat("Snow white", 15, true),
+                new Cat("Bella", 5, true),
+                new Cat("Luna", 2, true),
+                new Cat("Leo", 1, false));
+
+/*
+        int sum = integerList.stream()
+                .mapToInt(i -> i)
+                .filter(value -> value < 5)
+                .sum();
+
+        //System.out.println(sum);
+
+        long count = integerList.stream()
+                .mapToInt(i -> i)
+                .filter(StreamExamples::isOdd)
+
+//                Dessa 2 ger samma resultat:
+//                .filter(value -> isOdd(value))
+//                .filter(StreamExamples::isOdd)
+
+                .count();
+        //System.out.println(count);
+
+
+        long catCount1 = catList.stream()
+                .filter(Cat::female)
+                .count();
+        System.out.println(catCount1);
+
+        long maleCount = catList.stream()
+                .filter(not(Cat::female))
+                .filter(cat -> cat.age() < 5)
+                .count();
+        System.out.println(catCount1);
+
+        double average = catList.stream()
+                .mapToInt(Cat::age)
+                .average()
+                .orElseThrow();
+        System.out.println(average);
+
+        //Sort according to number of characters in cat name
+        var result = catList.stream()
+                .sorted(Comparator.comparingInt(Cat::getNameLength)
+                        .thenComparing(Cat::name))
+                .toList();
+
+        result.forEach(System.out::println);
+*/
+
+        catList.stream()
+                .map(Cat::name)
+                .map(String::toLowerCase)
+                .filter(name -> name.startsWith("l"))
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .forEach(System.out::println);
+
+
+    }
+
+    private static boolean lessThanFive(int value) {
+        return value < 5;
+    }
+
+    private static boolean isOdd(int value) {
+        return value % 2 == 1;
+    }
+}
+
+record Cat(String name, int age, boolean female) {
+    public int getNameLength() {
+        return name.length();
+    }
+}
